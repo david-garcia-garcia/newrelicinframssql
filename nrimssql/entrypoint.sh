a3339f3231b8ceaed91bd17b3384ffc8f3e99c81
deivid.garcia.garcia@gmail.com <deivid.garcia.garcia@gmail.com>
2024-07-04 08:00:21 +0200

    Initial commit

---
+++ nrimssql/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -eo pipefail
+
+rm -f /var/run/newrelic-infra/newrelic-infra.pid
+
+echo "Configuring /etc/newrelic-infra/integrations.d/mssql-config.yml from environment"
+
+HOSTNAME=${MSSQL_HOSTNAME}
+USERNAME=${MSSQL_USERNAME}
+PASSWORD=${MSSQL_PASSWORD}
+PORT=${MSSQL_PORT}
+
+sed -i "s/\${HOSTNAME}/$HOSTNAME/" /etc/newrelic-infra/integrations.d/mssql-config.yml
+sed -i "s/\${USERNAME}/$USERNAME/" /etc/newrelic-infra/integrations.d/mssql-config.yml
+sed -i "s/\${PASSWORD}/$PASSWORD/" /etc/newrelic-infra/integrations.d/mssql-config.yml
+sed -i "s/\${PORT}/$PORT/" /etc/newrelic-infra/integrations.d/mssql-config.yml
+
+echo "Configuring /etc/newrelic-infra.yml from environment"
+
+: "${NRI_LOGLEVEL:=info}"
+
+sed -i "s/\${LOGLEVEL}/$NRI_LOGLEVEL/" /etc/newrelic-infra.yml
+sed -i "s/\${NRI_LICENSE_KEY}/$NRI_LICENSE_KEY/" /etc/newrelic-infra.yml
+
+echo "Starting newrelic-infra as a foreground process"
+
+exec /usr/bin/newrelic-infra
---
+++ readme.md
@@ -0,0 +1,17 @@
+
+
+**Docker image with the MSSQL integration** for New Relic
+
+[Microsoft SQL Server monitoring integration | New Relic Documentation](https://docs.newrelic.com/es/docs/infrastructure/host-integrations/host-integrations-list/microsoft-sql/microsoft-sql-server-integration/)
+
+**Environment variables**
+
+| Name            | Description                         | Default Value |
+| --------------- | ----------------------------------- | ------------- |
+| MSSQL_HOSTNAME  | Database hostname                   | Empty         |
+| MSSQL_USERNAME  | Database username                   | Empty         |
+| MSSQL_PASSWORD  | Database password                   | Empty         |
+| MSSQL_PORT      | Database port                       | 1433          |
+| NRI_LOGLEVEL    | Log level for the NRI agent         | info          |
+| NRI_LICENSE_KEY | License key for the NRI integration | Empty         |
+
